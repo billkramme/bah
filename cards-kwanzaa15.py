68649fd602e7191e0d393113274c863100e87420
GreetingsADM <andrew.d.maynard@gmail.com>
2015-01-04 15:19:13 -0600

    Create cards-kwanzaa15.py
    Creation of Kwanzaa cards from 2015. There are 2014 sets and a potential for future sets so numbering convention was used.

---
+++ cards-kwanzaa15.py
@@ -0,0 +1,32 @@
+sets.append("kwanzaa15")
+wcards-kwanzaa15 = [
+#kwanzaaPast
+    "A protracted siege.",
+    "200 years of slavery.",
+    "Harnessing the miraculous power of the atom to slaughter 200,000 Japanese people.",
+    "Rock music and premarital sex.",
+    "This groovy new thing called LSD.",
+    "Blockbuster late fees up the wazoo.",
+    "The Bowflex Revolution.",
+    "Reading and entire book.",
+#kwanzaaPresent
+    "All the poop inside my body.",
+    "A vague fear of something called ISIS.",
+    "The diminishing purity of the white race.",
+    "Ebola.",
+    "Trying to feel something, anything.",
+    "Small-town cops with M4 assault rifles.",
+    "The 9000 children who starved to death today.",
+    "The transience of all things.",
+    ]
+bcards-kwanzaa15 = [
+#KwanzaaPast
+    "Dear Mom and Dad, Camp is fun. I like capture the flag. Yesterday, one of the older kids taught me about __________. I love you, Casey.",
+    "Honey, Mommy and Daddy love you very much. But apparently Mommy loves __________. more than she loves Daddy.",
+#KwanzaaPresent
+    "Why am I so tired?",
+    ]
+bcards2-kwanzaa15 = [
+#KwanzaaPresent
+    "Today on Buzzfeed: 10 pictures of __________ That Look Like __________!",
+]
