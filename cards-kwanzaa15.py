f985553a9087d386fa85b148f8dc033a465d7ce0
GreetingsADM <andrew.d.maynard@gmail.com>
2015-01-17 18:54:12 -0600

    Update cards-kwanzaa15.py
    Adding Kwanza future 2015

--- cards-kwanzaa15.py
+++ cards-kwanzaa15.py
@@ -18,6 +18,15 @@ wcards-kwanzaa15 = [
     "Small-town cops with M4 assault rifles.",
     "The 9000 children who starved to death today.",
     "The transience of all things.",
+#KwanzaaFuture
+    "What remains of my penis.",
+    "Rising sea levels consistent with scientific predictions.",
+    "Being replaced by a robot.",
+    "The events depicted in James Cameron's Avatar.",
+    "Building a ladder of hot dogs to the moon.",
+    "The Great Lizard Uprising of 2352.",
+    "A cloud of ash that darkens the Earth for a thousand years.",
+    "The dying breath of the last human.",
     ]
 bcards-kwanzaa15 = [
 #KwanzaaPast
@@ -25,8 +34,12 @@ bcards-kwanzaa15 = [
     "Honey, Mommy and Daddy love you very much. But apparently Mommy loves __________. more than she loves Daddy.",
 #KwanzaaPresent
     "Why am I so tired?",
+#KwanzaaFuture
+    "Behold the Four Hosemen of the Apocalypse! War, Famine, Death, and __________.",
     ]
 bcards2-kwanzaa15 = [
 #KwanzaaPresent
     "Today on Buzzfeed: 10 pictures of __________ That Look Like __________!",
+#KwanzaaFuture
+    "A curse upon thee! Many years from now, just when you think you're safe, __________ shall turn into __________."
 ]
